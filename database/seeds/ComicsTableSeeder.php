03e39616d6f2ca265998502d71ff0bc1d8b6171f
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-22 16:31:37 +0100

    Created Model, Migration and Seeder for Comics table

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $new_comic = new Comic();
+
+            $new_comic->fill($comic);
+
+            $new_comic->save();
+        }
+    }
+}
