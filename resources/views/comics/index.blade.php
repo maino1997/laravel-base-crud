bac1b2b4d1c8bb2f2389dc94bd9608360b2004d6
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-23 16:39:38 +0100

    added basic style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,37 +1,49 @@
 @extends('layouts.main')
 
 @section('content')
-    <ul>
-        @foreach ($comics as $comic)
-            <li>
-                Titolo: {{ $comic->title }}
-            </li>
-            <li>
-                Descrizione: {{ $comic->description }}
-            </li>
-            <li>
-                <a href="{{ route('comics.show', $comic->id) }}">
-                    <img src="{{ $comic->thumb }}" alt="image">
-                </a>
-            </li>
-            <li>
-                Prezzo: {{ $comic->price }}
-            </li>
-            <li>
-                Serie: {{ $comic->series }}
-            </li>
-            <li>
-                Data di rilascio: {{ $comic->sale_date }}
-            </li>
-            <li>
-                Tipo: {{ $comic->type }}
-            </li>
-            <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
-            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-                @method('DELETE')
-                @csrf
-                <button>Elimina</button>
-            </form>
-        @endforeach
-    </ul>
+    <div class="container">
+        <div class="row">
+
+            @foreach ($comics as $comic)
+                <div class="col-3">
+                    <ul class="card">
+
+                        <li>
+                            <h2>
+                                Titolo: {{ $comic->title }}
+                            </h2>
+                        </li>
+                        <li>
+                            <a href="{{ route('comics.show', $comic->id) }}">
+                                <img src="{{ $comic->thumb }}" alt="image">
+                            </a>
+                        </li>
+                        <li>
+                            <p>
+                                Descrizione: {{ $comic->description }}
+                            </p>
+                        </li>
+                        <li>
+                            Prezzo: {{ $comic->price }}
+                        </li>
+                        <li>
+                            Serie: {{ $comic->series }}
+                        </li>
+                        <li>
+                            Data di rilascio: {{ $comic->sale_date }}
+                        </li>
+                        <li>
+                            Tipo: {{ $comic->type }}
+                        </li>
+                        <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+                        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button>Elimina</button>
+                        </form>
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
