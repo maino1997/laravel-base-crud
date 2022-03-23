05186cca15116e4758313b6c8795d8154e9b28a0
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-23 16:47:11 +0100

    fixed typo on comic id edit page

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,20 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-    <form action="{{ route('comics.update', $comic->idphp) }}" method="POST">
+    <form action="{{ route('comics.update', $comic->id) }}" method="POST">
         @method('PUT')
         @csrf
-        <form action="{{ route('comics.store') }}" method="POST">
-            @csrf
-            <input type="text" name="title" id="title" placeholder="title" value="{{ $comic->title }}">
-            <input type="text" name="description" id="description" placeholder="description"
-                value="{{ $comic->description }}">
-            <input type="text" name="thumb" id="thumb" placeholder="immagine" value="{{ $comic->thumb }}">
-            <input type="text" name="price" id="price" placeholder="price" value="{{ $comic->price }}">
-            <input type="text" name="series" id="series" placeholder="series" value="{{ $comic->series }}">
-            <input type="text" name="sale_date" id="sale_date" placeholder="sale_date" value="{{ $comic->sale_date }}">
-            <input type="text" name="type" id="type" placeholder="type" value="{{ $comic->type }}">
-            <button type="submit">Invia</button>
-        </form>
+        <input type="text" name="title" id="title" placeholder="title" value="{{ $comic->title }}">
+        <input type="text" name="description" id="description" placeholder="description"
+            value="{{ $comic->description }}">
+        <input type="text" name="thumb" id="thumb" placeholder="immagine" value="{{ $comic->thumb }}">
+        <input type="text" name="price" id="price" placeholder="price" value="{{ $comic->price }}">
+        <input type="text" name="series" id="series" placeholder="series" value="{{ $comic->series }}">
+        <input type="text" name="sale_date" id="sale_date" placeholder="sale_date" value="{{ $comic->sale_date }}">
+        <input type="text" name="type" id="type" placeholder="type" value="{{ $comic->type }}">
+        <button type="submit">Invia</button>
     </form>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>
-        Titolo: {{ $c->title }}
-    </div>
-    <div>
-        Descrizione: {{ $c->description }}
-    </div>
-    <div>
-        <img src="{{ $c->thumb }}" alt="image">
-    </div>
-    <div>
-        Prezzo: {{ $c->price }}
-    </div>
-    <div>
-        Serie: {{ $c->series }}
-    </div>
-    <div>
-        Data di rilascio: {{ $c->sale_date }}
-    </div>
-    <div>
-        Tipo: {{ $c->type }}
+    <div class="container">
+        <div class="row">
+            <div class="content card">
+                <div>
+                    Titolo: {{ $c->title }}
+                </div>
+                <div>
+                    Descrizione: {{ $c->description }}
+                </div>
+                <div>
+                    <img src="{{ $c->thumb }}" alt="image">
+                </div>
+                <div>
+                    Prezzo: {{ $c->price }}
+                </div>
+                <div>
+                    Serie: {{ $c->series }}
+                </div>
+                <div>
+                    Data di rilascio: {{ $c->sale_date }}
+                </div>
+                <div>
+                    Tipo: {{ $c->type }}
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
