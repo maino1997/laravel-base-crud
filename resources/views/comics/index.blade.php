5e0e7d39bf93b895482d66f4d3da77c8756cbe46
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-28 19:02:03 +0200

    added markup table version

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,59 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+
+
+
+        {{-- MarkUp Table Version --}}
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Immagine</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Serie</th>
+                    <th scope="col">Data di Vendita</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <th scope="row">{{ $comic->id }}</th>
+                        <td>{{ $comic->title }}</td>
+                        <td><a href="{{ route('comics.show', $comic->id) }}">
+                                <img src="{{ $comic->thumb }}" alt="image">
+                            </a></td>
+                        <td>{{ $comic->description }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                        <td>{{ $comic->type }}</td>
+                        <td><a class="btn btn-primary m-2" href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+                            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" class="delete m-2"
+                                data-title="{{ $comic->title }}">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger">Elimina</button>
+                            </form>
+                        </td>
+
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+
+
+
+
+
+
+        {{-- MarkUp Card Version --}}
+        {{-- <div class="row">
             @foreach ($comics as $comic)
                 <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-4">
                     <ul class="carta">
@@ -45,7 +97,7 @@
                     </ul>
                 </div>
             @endforeach
-        </div>
+        </div> --}}
     </div>
 @endsection
 
